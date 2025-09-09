87bf2b94bba2a5d469234f66e2563e9449cadba3
biancapcr <biancacristina.pocora@gmail.com>
2025-09-09 10:14:43 +0200

    [feat] (add) navbar and layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <header className="topbar">
+      <div className="topbar-inner">
+        <NavLink to="/" className="brand">MyStore</NavLink>
+
+        <nav className="menu" aria-label="Main">
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) => `menu-link${isActive ? " is-active" : ""}`}
+          >
+            Homepage
+          </NavLink>
+
+          <NavLink
+            to="/prodotti"
+            className={({ isActive }) => `menu-link${isActive ? " is-active" : ""}`}
+          >
+            Prodotti
+          </NavLink>
+
+          <NavLink
+            to="/chisiamo"
+            className={({ isActive }) => `menu-link${isActive ? " is-active" : ""}`}
+          >
+            Chi Siamo
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+}
